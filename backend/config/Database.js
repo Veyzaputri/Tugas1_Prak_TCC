5da45d47b54e3e6c33e35f07f3d8f64c9d0b216d
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-03-10 11:05:25 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
-const db = new Sequelize ('notes','root','',{
-    host: 'localhost',
+const db = new Sequelize ('veyza-e-13','root','',{
+    host: '104.198.64.135',
     dialect: 'mysql'
 });
 
